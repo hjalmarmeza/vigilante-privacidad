1668c001d32234c69f3d07cf9c47a8f959631978
Hjalmar Meza <hjalmar.meza@gmail.com>
2026-03-16 14:41:16 +0100

    Security: Move Firebase config to separate file and prepare for API restrictions

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Configuración de Firebase - Vigilante de Privacidad
+// Nota: Esta clave identifica tu proyecto en el navegador. 
+// Para máxima seguridad, se recomienda restringir esta clave 
+// a tu dominio (hjalmarmeza.github.io) en la consola de Google Cloud.
+
+export const firebaseConfig = {
+    projectId: "vigilante-privacidad-hm",
+    appId: "1:648731153624:web:b2a981de5ecfcdd9a3b3ad",
+    storageBucket: "vigilante-privacidad-hm.firebasestorage.app",
+    apiKey: "AIzaSyDOhBaPa_rpWdSC_E4TpJWoJ-DnhRMAu0U",
+    authDomain: "vigilante-privacidad-hm.firebaseapp.com",
+    messagingSenderId: "648731153624"
+};
